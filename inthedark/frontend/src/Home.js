4c3edb899c298f3c972939ae47443d2cc6ddb2e9
shini <szhang93@ucsc.edu>
2020-04-26 16:05:36 -0700

    Input block added

--- inthedark/frontend/src/Home.js
+++ inthedark/frontend/src/Home.js
@@ -6,23 +6,138 @@ import Button from 'react-bootstrap/Button';
 import Container from 'react-bootstrap/Container';
 import Row from 'react-bootstrap/Row';
 import Col from 'react-bootstrap/Col';
-import ReactCSSTransitionGroup from 'react-transition-group';
+import InputGroup from 'react-bootstrap/InputGroup';
+import FormControl from 'react-bootstrap/FormControl';
+import Alert from 'react-bootstrap/Alert';
 // REFERENCES
 //https://stackoverflow.com/questions/6805482/css3-transition-animation-on-load
 
+const btnState = {NONE:0, CREATE:1, JOIN:2}
 
-
+class InputChatName extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      inputText: "",
+      alertMsg: "",
+      alertShow: false
+    }
+    this.btnSubmit = React.createRef();
+    this.buttonType = this.buttonType.bind(this)
+    this.submitClicked = this.submitClicked.bind(this)
+    this.checkEnter = this.checkEnter.bind(this)
+    this.inputChanged = this.inputChanged.bind(this)
+  }
+  buttonType() {
+    if(this.props.type == btnState.CREATE){return "Create"}
+    else if(this.props.type == btnState.JOIN){return "Join"}
+  }
+  checkEnter(e) {
+    if(e.key == "Enter"){
+      this.btnSubmit.current.click()
+    }
+  }
+  isInputValid(string) {
+    var pattern = /^(\d*\w*-*_*)+$/i
+    return (pattern.test(string))
+  }
+  submitClicked() {
+  }
+  inputChanged(e) {
+    if(this.isInputValid(e.target.value)){
+      this.setState({
+        inputText: e.target.value,
+        alertShow: false
+      })
+    }
+    else{
+      this.setState({
+          alertMsg: "Names can only include alphanumeric, '-', '_'. \
+          No spaces or special characters are allowed.",
+          alertShow: true
+      })
+    }
+  }
+  render() {
+    return(
+      <>
+        <div>
+        <Alert variant="danger" show={this.state.alertShow}>
+          {this.state.alertMsg}
+        </Alert>
+        </div>
+        <div>
+          <InputGroup>
+            <FormControl
+              size="lg"
+              onKeyPress={this.checkEnter}
+              placeholder={"Enter Room Name"}
+              onChange={this.inputChanged}
+            />
+            <InputGroup.Append>
+              <Button variant="outline-dark"
+              ref={this.btnSubmit}
+              onClick={this.submitClicked}
+              type="submit"
+              >{this.buttonType()}</Button>
+            </InputGroup.Append>
+          </InputGroup>
+        </div>
+      </>
+    )
+  }
+}
 
 class JoinChatButtons extends Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      btnPressed: btnState.NONE
+    }
+    this.btnPress = this.btnPress.bind(this)
+    this.inputBox = React.createRef()
+  }
+  btnPress(type, e) {
+    this.setState({btnPressed: type}, this.genInputBox)
+  }
+  genInputBox(){
+    if(this.state.btnPressed == btnState.CREATE){
+      return <InputChatName type={btnState.CREATE}/>
+    }
+    else if(this.state.btnPressed == btnState.JOIN){
+      return <InputChatName type={btnState.JOIN}/>
+    }
+    else{
+      return <></>
+    }
+  }
   render() {
     return(
       <Container>
         <Row className="justify-content-md-center">
           <Col>
-            <Button variant="dark" size="lg" className="BtnFloatRight">Create Room</Button>
+            <Button id="CreateBtn"
+            variant="dark"
+            size="lg"
+            className="createRoomBtn"
+            onClick={()=>this.btnPress(btnState.CREATE)}
+            >Create Room</Button>
           </Col>
           <Col>
-            <Button variant="info" size="lg" >Join Room</Button>
+            <Button id="JoinBtn"
+            variant="info"
+            size="lg"
+            className="joinRoomBtn"
+            onClick={()=>this.btnPress(btnState.JOIN)}
+            >Join Room</Button>
+          </Col>
+        </Row>
+        <Row className="rowGap"></Row>
+        <Row className="justify-content-md-center">
+          <Col>
+            <div className="InputBox">
+              {this.genInputBox()}
+            </div>
           </Col>
         </Row>
       </Container>
@@ -33,13 +148,14 @@ class JoinChatButtons extends Component {
 class Home extends Component {
   constructor(props) {
     super(props)
+    this.title = React.createRef();
+    this.joinButtons = React.createRef();
   }
   componentDidMount() {
-    window.onload = function() {
-      document.getElementById("homeTitle").className += " loaded"
-      document.getElementById("joinChatButtons").className += " loaded"
-    }
-
+    setTimeout(()=>{
+      this.title.current.className += " loaded"
+      this.joinButtons.current.className += " loaded"
+    }, 0)
   }
   render() {
     return (
@@ -47,14 +163,20 @@ class Home extends Component {
         <Container>
           <Row className="justify-content-md-center">
             <Col>
-              <div id="homeTitle" className="Title">
+              <div
+              className="Title"
+              ref={this.title}
+              >
                 <p>in the dark</p>
               </div>
             </Col>
           </Row>
           <Row className="justify-content-md-center">
             <Col>
-              <div id="joinChatButtons" className="JoinChatButtons">
+              <div
+              className="JoinChatButtons"
+              ref={this.joinButtons}
+              >
                 <JoinChatButtons />
               </div>
             </Col>
--- inthedark/frontend/src/index.js
+++ inthedark/frontend/src/index.js
@@ -4,8 +4,8 @@ import Home from './Home';
 import './index.css';
 
 import 'bootstrap/dist/css/bootstrap.min.css';
-
 export const API_URL = "http://localhost:8000/api/server/";
+
 ReactDOM.render(
   <Home />,
   document.getElementById('root')
